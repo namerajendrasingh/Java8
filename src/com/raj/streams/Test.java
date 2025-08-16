94dd0f90b3c30b0943eb5721c448a6fd6b9045b1
namerajendrasingh <rajendra.singh068@gmail.com>
2025-08-17 03:02:59 +0530

    Initial Commit on Java8 Project

---
+++ src/com/raj/streams/Test.java
@@ -0,0 +1,10 @@
+package com.raj.streams;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Setting Project!");
+	}
+
+}
