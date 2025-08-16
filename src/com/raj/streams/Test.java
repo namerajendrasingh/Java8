d596f3fed4ee5917d4c5c06cb319054e7d9ba9a2
namerajendrasingh <rajendra.singh068@gmail.com>
2025-08-17 03:42:53 +0530

    first stream implementation

--- src/com/raj/streams/Test.java
+++ src/com/raj/streams/Test.java
@@ -1,11 +1,17 @@
 package com.raj.streams;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-        System.out.println("Setting Project!");
-        System.out.println("==");
+		List<Integer> arr = Arrays.asList(1,2,3,4,5,6);
+		List<Integer> filterdList = arr.stream().filter(e->(e<4)).collect(Collectors.toList());
+		for (Integer integer : filterdList) {
+			System.out.println(integer);
+		}
 	}
 
 }
