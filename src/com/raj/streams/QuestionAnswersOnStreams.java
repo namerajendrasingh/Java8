f3c804b7aa10c7266047cc7ecf6e39807e3e79d9
namerajendrasingh <rajendra.singh068@gmail.com>
2025-08-19 22:09:32 +0530

    Working with Primitive Streams

--- src/com/raj/streams/QuestionAnswersOnStreams.java
+++ src/com/raj/streams/QuestionAnswersOnStreams.java
@@ -3,7 +3,9 @@ package com.raj.streams;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
 /**
@@ -18,14 +20,30 @@ public class QuestionAnswersOnStreams {
 		//Question: How to make and infinite stream into finite stream?
 		infiniteStreamIntoFinitStream();
 		
+		/*
+		 * Primitive Streams
+           Specialized streams for primitives 
+           (IntStream, DoubleStream, LongStream):
+		 */
 		
+		IntStream intStream = IntStream.of(1,2,3,4);
+		int sum = intStream.sum();
+		System.out.println("\n Sum:"+sum);
+		
+		LongStream longStream = LongStream.of(1,2,3,4);
+		long longSum = longStream.sum();
+		System.out.println("\n Long Stream Sum:"+longSum);
+		
+		DoubleStream doubleStream = DoubleStream.of(1,2,3,4);
+		double doubleSum = doubleStream.sum();
+		System.out.println("\n Double Stream Sum:"+doubleSum);
 				
 	}
 
 	private static void infiniteStreamIntoFinitStream() {
      Stream<Integer> infiniteStream = Stream.iterate(0, n->n+2);	
      //Now making stream finit stream 
-     infiniteStream.limit(10).forEach(System.out::println);
+     infiniteStream.limit(10).forEach(n->{System.out.print(n+",");});
 	}
 
 	private static void mergeTwoStreams() {
@@ -33,7 +51,6 @@ public class QuestionAnswersOnStreams {
 		Stream<String> stream2 = Stream.of("c","d");
 		Stream<String> mergeTwoStreams = Stream.concat(stream1, stream2);
 		mergeTwoStreams.forEachOrdered(e->System.out.println(e));// TODO Auto-generated method stub
-		
+	    	
 	}
-
 }
